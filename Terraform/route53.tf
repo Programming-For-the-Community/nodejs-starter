7206d4e1c8349f73cbaf9c79d01dabc1df6f15fb
ProfessorChaos0802 <1822rhit@pm.me>
2024-11-28 13:45:58 -0600

    Adding CNAME record for cert to Route53

--- Terraform/route53.tf
+++ Terraform/route53.tf
@@ -8,3 +8,11 @@ resource "aws_route53_record" "professorchaos0802_record" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "professorchaos0802_certificate_record" {
+  zone_id = var.hosted_zone_id
+  name    = aws_acm_certificate.nodejs-starter-frontend-certificate.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.nodejs-starter-frontend-certificate.domain_validation_options[0].resource_record_type
+  ttl     = 60
+  records = [aws_acm_certificate.nodejs-starter-frontend-certificate.domain_validation_options[0].resource_record_value]
+}
