1bbf1ecd10a94620ce403284e41d69a0ec51ad90
ProfessorChaos0802 <106257169+ProfessorChaos0802@users.noreply.github.com>
2024-11-29 16:25:30 -0600

    Pulling in most current tf/dev (#5)
    * trying ECS without LB
    
    * re-introducting the load balancer
    
    * Updating CPU/Mem config
    
    * Updating SGs from port 80 to port 8080
    
    * Updating HTTP Load Balancer SG to allow ingress from all IPs and all ports
    
    * placing the LB in public subnet instead of private
    
    * fixing missing stuff
    
    * Fixing subnet cidrs
    
    * Trying again
    
    * Testing new pipeline
    
    * hopefully working
    
    * Setting up custom DNS Hosting
    
    * Fixing lb error and hosted zone
    
    * fixing routing rules
    
    * fixing healthcheck path
    
    * Fixing typo
    
    * Fiddling with Route53, reverting LB
    
    * Making LB HTTPS
    
    * Adding CNAME record for cert to Route53
    
    * Adding record CNAME
    
    * new cert record
    
    * Fixing certficate and redirecting http traffic
    
    * Forgot to run a tf format
    
    * Updating README and Changing teardown workflow name

--- Terraform/load-balancers.tf
+++ Terraform/load-balancers.tf
@@ -1,16 +1,16 @@
 resource "aws_lb" "nodejs_starter_frontend_lb" {
-  name               = "nodejs-starter-frontend-ecs-lb"
-  internal           = false
-  load_balancer_type = "application"
+  name                             = "nodejs-starter-frontend-ecs-lb"
+  internal                         = false
+  load_balancer_type               = "application"
+  enable_cross_zone_load_balancing = true
 
   security_groups = [
     aws_security_group.nodejs_starter_frontend_elb_sg.id
   ]
 
   subnets = [
-    aws_subnet.nodejs_starter_east2a.id,
-    aws_subnet.nodejs_starter_east2b.id,
-    aws_subnet.nodejs_starter_east2c.id
+    aws_subnet.nodejs_starter_public1.id,
+    aws_subnet.nodejs_starter_public2.id
   ]
 
   lifecycle {
@@ -25,9 +25,9 @@ resource "aws_lb" "nodejs_starter_frontend_lb" {
   }
 }
 
-resource "aws_lb_target_group" "nodejs_starter_frontend_lb_tg_http" {
-  name        = "node-starter-frontend-tg-http"
-  port        = 80
+resource "aws_lb_target_group" "nodejs_starter_frontend_lb_tg" {
+  name        = "node-starter-frontend-tg"
+  port        = 8080
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = var.vpc_id
@@ -50,71 +50,38 @@ resource "aws_lb_target_group" "nodejs_starter_frontend_lb_tg_http" {
   }
 }
 
-# resource "aws_lb_target_group" "nodejs_starter_frontend_lb_tg_https" {
-#   name        = "node-starter-frontend-tg-https"
-#   port        = 443
-#   protocol    = "HTTPS"
-# #   target_type = "ip"
-#   vpc_id      = var.vpc_id
-
-#   health_check {
-#     path                = "/health"
-#     enabled             = true
-#     healthy_threshold   = 2
-#     unhealthy_threshold = 10
-#     interval            = 30
-#     matcher             = "200"
-#     timeout             = 10
-#   }
-
-#   tags = {
-#     Name        = "NodeJS Starter Front-End ECS Load Balancer Target Group HTTPS"
-#     project     = var.project
-#     owner       = var.owner
-#     environment = var.environment
-#   }
-# }
-
-# resource "aws_lb_target_group_attachment" "nodejs_starter_frontend_lb_tg_attachment" {
-#   target_group_arn = aws_lb_target_group.nodejs_starter_frontend_lb_tg.arn
-#   target_id        = aws_ecs_service.nodejs_starter_frontend_service.id
-#   port             = 8080
-# }
-
-resource "aws_lb_listener" "nodejs_starter_frontend_lb_listener_http" {
+resource "aws_lb_listener" "nodejs_starter_frontend_lb_listener" {
   load_balancer_arn = aws_lb.nodejs_starter_frontend_lb.arn
-  port              = 80
-  protocol          = "HTTP"
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.nodejs-starter-frontend-certificate.arn
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.nodejs_starter_frontend_lb_tg_http.arn
+    target_group_arn = aws_lb_target_group.nodejs_starter_frontend_lb_tg.arn
   }
 
   tags = {
-    Name        = "NodeJS Starter Front-End ECS Load Balancer Listener HTTP"
+    Name        = "NodeJS Starter Front-End ECS Load Balancer Listener HTTPS"
     project     = var.project
     owner       = var.owner
     environment = var.environment
   }
 }
 
-# resource "aws_lb_listener" "nodejs_starter_frontend_lb_listener_https" {
-#   load_balancer_arn = aws_lb.nodejs_starter_frontend_lb.arn
-#   port              = 443
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = aws_acm_certificate.my_certificate.arn  # Replace with your ACM certificate ARN
+resource "aws_lb_listener" "nodejs_starter_http_redirect_listener" {
+  load_balancer_arn = aws_lb.nodejs_starter_frontend_lb.arn
+  port              = 80
+  protocol          = "HTTP"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.nodejs_starter_frontend_lb_tg_https.arn
-#   }
+  default_action {
+    type = "redirect"
 
-#   tags = {
-#     Name        = "NodeJS Starter Front-End ECS Load Balancer Listener HTTPS"
-#     project     = var.project
-#     owner       = var.owner
-#     environment = var.environment
-#   }
-# }
+    redirect {
+      protocol    = "HTTPS"
+      port        = "443"
+      status_code = "HTTP_301"
+    }
+  }
+}
--- Terraform/nat-gateways.tf
+++ Terraform/nat-gateways.tf
@@ -4,7 +4,7 @@ resource "aws_eip" "nodejs_starter_nat_eip" {
 
 resource "aws_nat_gateway" "nodejs_starter_nat_gateway" {
   allocation_id = aws_eip.nodejs_starter_nat_eip.id
-  subnet_id     = aws_subnet.nodejs_starter_public.id
+  subnet_id     = aws_subnet.nodejs_starter_public1.id
 
   tags = {
     Name        = "NodeJS Starter NAT Gateway"
