5530a029c8359fab8f7b59db29919f10f509358f
ProfessorChaos0802 <1822rhit@pm.me>
2024-11-30 16:18:56 -0600

    Updating S3 bucket policy

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -8,3 +8,27 @@ resource "aws_s3_bucket" "nodejs_starter_frontend_lb_logs" {
     owner       = var.owner
   }
 }
+
+resource "aws_s3_bucket_policy" "nodejs_starter_lb_logs_policy" {
+  bucket = aws_s3_bucket.nodejs_starter_lb_logs.bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowLBLogDelivery"
+        Effect = "Allow"
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        }
+        Action   = "s3:PutObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.nodejs_starter_lb_logs.bucket}/*"
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = var.account_id # Replace with your AWS Account ID
+          }
+        }
+      }
+    ]
+  })
+}
